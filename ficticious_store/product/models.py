9df4ebc2bfd3984cbd643f283b9a34b18502279c
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-08-22 02:08:34 -0300

    [backend] -> (db.sqlite3, product/migrations/001_initial.py, product/models.py, and product/templates/index.html, at /ficticious_store) -> create product model and setting up migrations for the first time

--- ficticious_store/product/models.py
+++ ficticious_store/product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+
+	name = models.CharField('Name', max_length=100)	
+
+	price = models.DecimalField('Price', decimal_places=2, max_digits=8)
+
+	quantity = models.IntegerField('Quantity in Inventory')
