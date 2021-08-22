b8314c191ed0101da35eed317746f047734c4cab
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-08-21 23:53:04 -0300

    [backend] -> (templates/index.html, templates/product.html, and views.py, at ficticious_store/product/) -> setting up basic index and product templates

--- ficticious_store/product/views.py
+++ ficticious_store/product/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+	"""Index View fot the virtual ficticious store"""
 
-	return render(request, 'index.html')
+	context = {'keyword_01': 'value_01', 'keyword_02': 'value_02'}
+
+	return render(request, 'index.html', context)
 
 def product(request):
 
