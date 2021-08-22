0b46f30ec139b6fc26aa6cf186573317d1391e7d
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-08-21 22:22:24 -0300

    [backend] -> (settings.py, and urls.py, at ficticious_store/ficticious_store/) -> setting index and product views

--- ficticious_store/ficticious_store/urls.py
+++ ficticious_store/ficticious_store/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from product.views import index, product
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('product', product)
 ]
