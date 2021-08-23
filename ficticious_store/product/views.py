879b34fcbb51edf1a803ae3348e85225890bdb83
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-08-23 15:07:04 -0300

    [backend] -> (db.sqlite3, at /ficticious_store/; urls.py, views.py, templates/index.html, and templates/views.py, at ficticious_store/product/) -> until send data from database to templates, and open each product in its specific page

--- ficticious_store/product/views.py
+++ ficticious_store/product/views.py
@@ -1,14 +1,63 @@
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
 
 def index(request):
 	"""Index View fot the virtual ficticious store"""
 
-	context = {'keyword_01': 'value_01', 'keyword_02': 'value_02'}
+	# print(request)
+
+	# print(f'method: {request.method}')
+
+	# print(f'headers: {request.headers}')
+
+	# print(f"User-Agent: {request.headers['User-Agent']}")
+
+	# print(f"User: {request.user}")
+
+	logged: str = None
+
+	if request.user == 'AnonymousUser':
+
+		print('User not Logged')
+
+		logged = 'Not Logged'
+
+	else:
+
+		print('User Logged')
+
+		logged = 'Logged'
+
+	# print(f"user.last_name: {request.user.last_name}")
+
+	# print(f"user.email {request.user.email}")
+
+	# print(f"user.is_staff (is part of the team - like superuser): {request.user.is_staff}")
+
+	# print(dir(request))
+
+	# print(dir(request.user))
+
+	products = Product.objects.all()
+
+	context = {
+		'logged': logged,
+		'products': products
+	}
 
 	return render(request, 'index.html', context)
 
-def product(request):
+def product(request, pk):
 
-	return render(request, 'product.html')
+	product_obj = Product.objects.get(pk=pk)
+
+	context = {
+
+		'product': product_obj
+
+	}
+
+	return render(request, 'product.html', context)
